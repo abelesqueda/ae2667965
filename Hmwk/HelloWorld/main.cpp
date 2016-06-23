053b236ced0bdd95b745f3a72ffb39381a44c36c
abelesqueda <abel3289@yahoo.com>
2016-06-22 19:11:20 -0700

    Signed-off-by: abelesqueda <abel3289@yahoo.com>

---
+++ Hmwk/HelloWorld/main.cpp
@@ -0,0 +1,32 @@
+/* 
+ * File:   main.cpp
+ * Author: Abel S. Esqueda
+ * Created on June 20, 2016, 6:51 PM
+ * Purpose: First Program
+ */
+
+//System Libraries
+#include <iostream>  //Input/Output Library
+using namespace std; //Namespace of the System Libraries
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+
+    //Input Data
+    
+    //Process Data
+    
+    //Output the processed Data
+    cout<<"Hello World"<<endl;
+    
+    //Exit Stage Right!
+    return 0;
+}
+
